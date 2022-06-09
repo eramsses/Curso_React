ed02319b3812f0a2778e1fe958453ee9c9d0e259
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-09 00:55:04 -0600

    Insertado de logo

--- contador-clics/src/App.js
+++ contador-clics/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import logo from './imagenes/FreeCodeCamp_logo.png'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+        <div className='logo-contenedor'>
+          <img 
+          className='logo'
+          src={logo}
+          alt='Logo FreeCodeCamp' 
+          width={500}/>
+        </div>
     </div>
   );
 }
