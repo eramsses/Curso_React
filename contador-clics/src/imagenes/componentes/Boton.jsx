1f44d61eb91410d50e5343a7419f585721eaba65
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-09 01:08:20 -0600

    Asignar event Listener
    Que va a hacer el componente Boton al hacer clic

--- contador-clics/src/imagenes/componentes/Boton.jsx
+++ contador-clics/src/imagenes/componentes/Boton.jsx
@@ -1,9 +1,10 @@
 import React from React;
 
-function Boton({texto, esBotonDeClic}){
+function Boton({texto, esBotonDeClic, manejarClic}){
   return(
     <button 
-    className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' } >
+    className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+    onClick={manejarClic} >
       {texto}
     </button>
   );
