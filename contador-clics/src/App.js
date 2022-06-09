a50e8784efe57ab56bb76df2b9bddfc0a125447c
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-09 01:47:57 -0600

    Implementación de componente Contador

--- contador-clics/src/App.js
+++ contador-clics/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import './css/Boton.css'
+import './css/Contador.css'
 import logo from './imagenes/FreeCodeCamp_logo.png'
 import Boton from './componentes/Boton'
+import Contador from './componentes/Contador';
 
 const manejarClic = () => {
   console.log("Clic");
@@ -22,11 +24,12 @@ function App() {
           width={500}/>
         </div>
         <div className='contenedor-principal'>
+          <Contador numClics='5' />
           <Boton
             texto='Clic'
             esBotonDeClic={true}
             manejarClic={manejarClic}
-          ></Boton>
+          />
           <Boton
             texto='Reiniciar'
             esBotonDeClic={false}
