b16c2ea5b6f96ca3aaf26a0ac35a5c868b072cad
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-09 22:01:07 -0600

    Instalación de paquete de iconos

---
+++ todo-list/src/componentes/TareaFormulario.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../css/TareaFormulario.css'
+
+function TareaFormulario(props){
+    return (
+        <form>
+            <input
+                className="tarea-input"
+                type='text'
+                placeholder='Escribe una tarea'
+                name='tarea'
+            />
+            <button className="tarea-boton">
+                Agregar
+            </button>
+        </form>
+    );
+}
+
+export default TareaFormulario;
