29e47374f6a0bb0ecf39bc845288db719597c9d3
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-06-10 01:59:21 -0600

    Cambio en componente App

--- contador-clics-class-comp/src/App.js
+++ contador-clics-class-comp/src/App.js
@@ -4,9 +4,61 @@ import './css/Contador.css'
 import logo from './imagenes/FreeCodeCamp_logo.png'
 import Boton from './componentes/Boton'
 import Contador from './componentes/Contador';
-import { useState } from 'react';
+import React from 'react';
 
-function App() {
+class App extends React.Component{
+  
+ constructor() {
+    super();
+    this.state = {
+      numClics: 1
+    };
+
+    //bind asocia el método para que sean usados como metodos
+    this.manejarClic = this.manejarClic.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
+  }
+    manejarClic() {
+      this.setState(({ numClics }) => ({ numClics: numClics + 1 }));
+    }
+
+    reiniciarContador(){
+      this.setState({ numClics: 0});
+    }
+
+ 
+
+  render(){
+    return (
+      <div className='App'>
+          <div className='logo-contenedor'>
+            <img 
+            className='logo'
+            src={logo}
+            alt='Logo FreeCodeCamp' 
+            width={500}/>
+          </div>
+          <div className='contenedor-principal'>
+            <Contador numClics={this.state.numClics} />
+            <Boton
+              texto='Clic'
+              esBotonDeClic={true}
+              manejarClic={this.manejarClic}
+            />
+            <Boton
+              texto='Reiniciar'
+              esBotonDeClic={false}
+              manejarClic={this.reiniciarContador}
+            ></Boton>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+
+/*function App() {
   const [numClics, setNumClics] = useState(0);
 
   const manejarClic = () => {
@@ -41,6 +93,6 @@ function App() {
         </div>
     </div>
   );
-}
+}*/
 
 export default App;
